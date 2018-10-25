52c78626e8edf740f44f7702470779dc75a60b27
Michael Scheuchenpflug <michaelscheuchenpflug@gmx.at>
2018-10-25 13:09:50 +0200

    programm finished

--- convert.js
+++ convert.js
@@ -1,1 +1,6 @@
-console.log('Not implemented yet!');
+const chalk = require('chalk');
+const convert = require('convert-units');
+process.argv.splice(0, 2);
+if(process.argv.length != 4) console.error(chalk.red('Wrong number of parameters!'));
+if(isNaN(process.argv[0]) || process.argv[2] != 'to') console.error(chalk.red('Invalid parameters'));
+console.log(chalk.blue(convert(process.argv[0]).from(process.argv[1]).to(process.argv[3])));
